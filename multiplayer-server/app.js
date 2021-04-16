2d9ca8a027bf36027cc9b8ec12f7bf4eefdd74f4
Niels Christian Konrad Nielsen <mail@nckn.dk>
2021-04-16 23:46:58 +0200

    Wanting to deploy site again after rearranging git

---
+++ multiplayer-server/app.js
@@ -0,0 +1,138 @@
+const express = require('express')();
+const http = require('http').Server(express);
+const socketio = require('socket.io')(http);
+
+// socketio.of('/chat').clients((error, clients) => {
+//   if (error) throw error;
+//   console.log(clients); // => [PZDoMHjiu8PYfRiKAAAF, Anw2LatarvGVVXEIAAAD]
+// });
+
+// var clients = socketio.sockets.clients();
+// var clients = socketio.sockets.clients('room'); // all users from room `room`
+
+var position = {
+  x: 100,
+  y: 100
+}
+
+var color = 0
+var id = 0
+var clients =[];
+var sortedSockets = []
+
+socketio.on('connection', socket => {
+  var socketPair = {
+    // id: id,
+    id: socket.id
+  }
+
+  // When move is detected
+  socket.on('move', data => {
+    // Mouse movement
+    position.x = data.x
+    position.y = data.y
+    // console.log('x is: ', data)
+    // console.log('clients: ', clients)
+    socketio.emit('position', position)
+
+    // var total = socketio.engine.clientsCount;
+    // socketio.emit('getCount', total)
+    
+  })
+
+  socket.on('changeAllColors', data => {
+    // Mouse movement
+    color = data.color
+    console.log('id of it: ', data.id)
+    var obj = {color: color, id: data.id}
+    clients.forEach((element, index) => {
+      if (element.clientId === data.id) {
+        console.log('its a match')
+        element.color = color
+        obj = {color: color, id: element.clientId}
+      }
+    });
+    socketio.emit('color', obj)
+  })
+
+  // Store client info
+  socket.on('storeClientInfo', function (data) {
+    var clientInfo = new Object();
+    clientInfo.customId = data.customId;
+    clientInfo.clientId = socket.id;
+    // console.log('client Info: ', clientInfo)
+    // clientInfo.hooray = 'yay'
+    clients.push(clientInfo);
+    // Store client info
+    // socketio.emit('storeInfo', clientInfo)
+
+    // var my_id = id; //my_id = value for this exact socket connection
+    id++; //increment global id for further connnections
+    // Tell clients that there is a new one in town
+    socket.emit('user_connected', socketPair)
+    
+    // Send all clients
+    // socket.emit('all_clients', clients)
+
+    // Broadcast to all
+    var joining = {
+      joinId: socket.id,
+      clients: clients,
+    }
+    socketio.sockets.emit('broadcast', joining)
+    
+    return
+    // return clientInfo
+  });
+
+  socket.on('disconnect', function (data) {
+    // console.log(`someone is leaving: ${socket.id}`)
+    for (var i = 0, len = clients.length; i < len; ++i ) {
+      var c = clients[i];
+      if (c.clientId == socket.id) {
+        clients.splice(i, 1);
+        console.log('client id thats getting rid of: ', c.clientId)
+        // Send message that someone left
+        socket.emit('client_leaving', c.clientId)
+        // console.log(`someone is leaving: ${socket.id}`)
+        var package = {
+          leavingId: c.clientId,
+          remainers: clients
+        }
+        socketio.sockets.emit('broadcast', package)
+        break;
+      }
+    }
+
+    // Send all clients
+    socket.emit('all_clients', clients)
+    
+  });
+
+})
+
+// http.listen(3000, '192.168.0.101');
+http.listen(3000, () => {
+  console.log(`Server listening on 3000`);
+  // console.log(`Server listening on http://${host}:${port}`);
+});
+
+// Button movement
+// switch (data) {
+//   case 'left':
+//     position.x -= 5
+//     socketio.emit('position', position)
+//     break
+//   case 'right':
+//     position.x += 5
+//     socketio.emit('position', position)
+//     break
+//   case 'up':
+//     position.y -= 5
+//     socketio.emit('position', position)
+//     break
+//   case 'down':
+//     position.y += 5
+//     socketio.emit('position', position)
+//     break
+// }
